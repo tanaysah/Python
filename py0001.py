df366bf1dc187020ae6df44d5e79319569e70704
Tanay Sah <sah_cse@hotmail.com>
2026-02-06 12:59:13 +0530

    Add files via upload

---
+++ py0001.py
@@ -0,0 +1,15 @@
+'''
+
+| Escape Sequence | Meaning         | Example                      | Output          |
+| --------------- | --------------- | ---------------------------- | --------------- |
+| `\n`            | New line        | `print("Hello\nPython")`     | Hello<br>Python |
+| `\t`            | Horizontal tab  | `print("Hello\tPython")`     | Hello    Python |
+| `\\`            | Backslash       | `print("C:\\Python")`        | C:\Python       |
+| `\'`            | Single quote    | `print('It\'s Python')`      | It's Python     |
+| `\"`            | Double quote    | `print("He said \"Hello\"")` | He said "Hello" |
+| `\b`            | Backspace       | `print("Helloo\b")`          | Hello           |
+| `\r`            | Carriage return | `print("Hello\rPython")`     | Python          |
+| `\f`            | Form feed       | `print("Hello\fPython")`     | Hello Python    |
+| `\v`            | Vertical tab    | `print("Hello\vPython")`     | Hello Python    |
+
+'''
