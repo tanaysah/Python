6a082451bc6936057a1c6434715cb3eec96599a9
Tanay Sah <sah_cse@hotmail.com>
2026-02-06 12:57:59 +0530

    Add files via upload

---
+++ py11.11.py
@@ -0,0 +1,6 @@
+rows = int(input("Enter the rows: "))
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(i, end="")
+    print()
---
+++ py12.12.py
@@ -0,0 +1,6 @@
+# User input for number of rows
+rows = int(input("Enter the rows: "))
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
