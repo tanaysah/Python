32d4d345ed7fb3b8677d63f7238567dab1452658
Tanay Sah <sah_cse@hotmail.com>
2026-02-06 12:59:51 +0530

    Add files via upload

---
+++ py0005.py
@@ -0,0 +1,6 @@
+s1 = "Python123"
+s2 = "123"
+
+print(s1.isalpha())      # False
+print(s2.isdigit())      # True
+print(s1.isalnum())      # True
---
+++ py0006.py
@@ -0,0 +1,3 @@
+print(len(text))         # length of string
+print(text[0])           # first character
+print(text[-1])          # last character
