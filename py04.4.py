bfbc6a393acaac3403d6b95057ce2084f5cb203e
Tanay Sah <sah_cse@hotmail.com>
2026-02-06 12:57:09 +0530

    Add files via upload

---
+++ py04.4.py
@@ -0,0 +1,3 @@
+a = 2
+b = 330
+print("A") if a > b else print("B") if a < b else print("C")
---
+++ py05.5.py
@@ -0,0 +1,9 @@
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+if a > b:
+    print("A")
+elif a == b:
+    print("=")
+else:
+    print("B")
