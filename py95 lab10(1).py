a5270a0ab09947f3837edf67221d1e011ddfa7b3
Tanay Sah <sah_cse@hotmail.com>
2026-05-05 22:14:18 +0530

    Add files via upload

--- py95 lab10(1).py
+++ py95 lab10(1).py
@@ -4,7 +4,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Simple Window")
 root.geometry("400x300")
-root.resizable(False, False)
+root.resizable(True, True)
 
 label = tk.Label(root, text="Welcome to Tkinter GUI", font=("Arial", 14))
 label.pack(pady=100)
